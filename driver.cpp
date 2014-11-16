d3383f67d87ef51f0f7f9668dd3f41f68a6a1657
Jeffrey <jeffrey@simonsolutions.com>
2014-11-16 14:41:26 -0600

    Added check matrix index function

--- driver.cpp
+++ driver.cpp
@@ -2,17 +2,25 @@
 #include <vector>
 using namespace std;
 
-checkColumn(int i);
+void checkColumn(int, int, vector< vector<int> >&);
 
 int main()
 {
 
+	// Example Matrix: Allocation for 7.3
+	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	//  		Col1 Col2 Col3 Col4
+	//            A   B    C    D
+	//Row 1 : P0  0   0    1    2
+	//Row 2 : P1  1   0    0    0
+	//Row 3 : P2  1   3    5    4
+
 	vector<int> column(4);
 
 	column[0] = 0;
-	column[1] = 7;
-	column[2] = 5;
-	column[3] = 0;
+	column[1] = 0;
+	column[2] = 1;
+	column[3] = 2;
 
 	vector< vector <int > > row(1, column);
 
@@ -27,12 +35,12 @@ int main()
 	}
 
 	// check row 1, index 3
-	checkColumn(1, 3);
+	checkColumn(0, 3, row);
 
 	return 0;	
 }
 
-checkColumn(int rowIndex, int columnIndex, vector<int> * row)
+void checkColumn(int rowIndex, int columnIndex, vector< vector<int> > &matrix)
 {
-	cout << << endl;
+	cout << "Index (" << rowIndex << "," << columnIndex << ") contains: " << matrix[rowIndex][columnIndex] << endl;
 }
